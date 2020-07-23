73d95ac9fa0b00ae57d25883f85def2868d2a9f7
Fallopio <kibaryshnikov@yandex.ru>
2020-07-23 20:12:31 +0400

    changed path

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ mongoose.connect('mongodb+srv://Cyril:inglorion9312@simplechat-krolc.mongodb.net
 
 app.use(express.static(path.join(__dirname, 'build')));
 
-app.get('/SimpleAnimeSearch*', function (req, res) {
+app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
